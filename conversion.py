c51546f36ecfeeea7f583e1b76def7786865ba82
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-05-05 20:19:22 -0300

    conversion

---
+++ conversion.py
@@ -0,0 +1,53 @@
+#conversão
+
+#conversões ara string
+bool_string = str(True)
+print(bool_string)
+bool_string2 = str(False)
+print(bool_string2)
+int_string = str(5)
+print(int_string)
+float_string = str(3.14159)
+
+#conversões para int
+string_int = int("2019")
+print(string_int)
+bool_int = int(True)
+print(bool_int)
+bool_int2 = int(False)
+print(bool_int2)
+float_int = int(3.14159)
+print(float_int)
+string_int2 = int("101", 2)
+print(string_int2)
+string_int3 = int("F0A23B", 16)
+print(string_int3)
+
+#conversões para float
+
+string_float = float("3.14159")
+print(string_float)
+string_float2 = float("3")
+print(string_float2)
+bool_float = float(True)
+print(bool_float)
+bool_float2 = float(False)
+print(bool_float2)
+int_float = float(15)
+print(int_float)
+
+#conversões para bool
+string_bool = bool("Python")
+print(string_bool)
+string_bool2 = bool("True")
+print(string_bool2)
+string_bool3 = bool("False")
+print(string_bool3)
+int_bool = bool(17)
+print(int_bool)
+int_bool2 = bool(0)
+print(int_bool2)
+float_bool = bool(3.14)
+print(float_bool)
+float_bool2 = bool(0.0)
+print(float_bool2)
